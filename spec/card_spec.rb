2f863407dde6fa2f749081e571cb97f21408c7ae
Ted Staros <tstaros23@gmail.com>
2022-08-23 18:10:02 -0600

    Add before hooks to card class

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,17 +1,16 @@
 require './lib/card'
 
 RSpec.describe Card do
+  before(:each) do
+    @card = Card.new(:diamond, 'Queen', 12)
+  end
   it "exists" do
-    card = Card.new(:diamond, 'Queen', 12)
-
-    expect(card).to be_an_instance_of(Card)
+    expect(@card).to be_an_instance_of(Card)
   end
 
   it "has readable attributes" do
-    card = Card.new(:diamond, 'Queen', 12)
-
-    expect(card.suit).to eq(:diamond)
-    expect(card.value).to eq('Queen')
-    expect(card.rank).to eq(12)
+    expect(@card.suit).to eq(:diamond)
+    expect(@card.value).to eq('Queen')
+    expect(@card.rank).to eq(12)
   end
 end
