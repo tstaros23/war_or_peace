54b16532df70b08449e25605e17611abe66a41ea
Ted Staros <tstaros23@gmail.com>
2022-08-23 21:07:08 -0600

    Finish first iteration

--- lib/deck.rb
+++ lib/deck.rb
@@ -21,4 +21,12 @@ class Deck
   def percent_high_ranking
     ((high_ranking_cards.count.to_f / cards.count.to_f) * 100).round(2)
   end
+
+  def remove_card
+    @cards.shift
+  end
+
+  def add_card(card)
+    @cards << card
+  end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -4,11 +4,11 @@ require './lib/deck'
 
 RSpec.describe Deck do
   before(:each) do
-    card1 = Card.new(:diamond, 'Queen', 11)
-    card2 = Card.new(:spade, '3', 10)
-    card3 = Card.new(:heart, 'Ace', 14)
+    @card1 = Card.new(:diamond, 'Queen', 11)
+    @card2 = Card.new(:spade, '3', 10)
+    @card3 = Card.new(:heart, 'Ace', 14)
 
-    @cards = [card1, card2, card3]
+    @cards = [@card1, @card2, @card3]
 
     @deck = Deck.new(@cards)
   end
@@ -27,10 +27,20 @@ RSpec.describe Deck do
   end
 
   it "find high ranking cards" do
-       expect(@deck.high_ranking_cards).to eq ([@cards[0], @cards[2]])
+       expect(@deck.high_ranking_cards).to eq ([@card1, @card3])
   end
 
-  it "find high ranking cards" do
+  it "can find the percentage of the high ranking cards in the deck" do
     expect(@deck.percent_high_ranking).to eq (66.67)
+  end
+
+  it "can remove a card from the top of the deck and add a card to the bottom of the deck" do
+    @deck.remove_card
+    expect(@deck.cards).to eq([@card2, @card3])
+    card4 = Card.new(:club, '5', 5)
+    @deck.add_card(card4)
+    expect(@deck.cards).to eq([@card2, @card3, card4])
+    expect(@deck.high_ranking_cards).to eq([@card3])
+    expect(@deck.percent_high_ranking).to eq(33.33)
   end
 end
